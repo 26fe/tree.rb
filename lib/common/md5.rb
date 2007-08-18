5c980dcbf80cf4cffc51c89cd164019dffc1fc47
Giovanni Ferro <giovanni.ferro@gmail.com>
2007-08-18 09:31:45 +0000

    add md5 utility
    git-svn-id: svn+ssh://hestia.dreamhost.com/home/gferro_cm/svn.repo/projects/common/trunk@127 4aeedc48-2d20-0410-9d33-d2643f36798d

---
+++ lib/common/md5.rb
@@ -0,0 +1,14 @@
+#
+# calcola md5 di file grandi, copiata da usenet
+#
+class MD5
+  def self.file(file)
+    File.open(file, "rb") do |f|
+      res = self.new
+      while (data = f.read(4096))
+        res << data
+      end
+      res
+    end
+  end
+end
