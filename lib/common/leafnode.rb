7a0206ed5263b67097171be9f88ef6b66f33c9d1
Tokiro <tokiro.oyama@gmail.com>
2007-08-25 07:43:58 +0000

    moving from svn repository (dreamhost) to git

--- lib/common/leafnode.rb
+++ lib/common/leafnode.rb
@@ -14,10 +14,25 @@ class LeafNode < AbsNode
     if parent
       parent.add_leaf( self ) 
     end
+    @path_from_root = nil
   end
 
   def accept( visitor )
     visitor.visit_leafNode( self )
+  end
+
+  def path_from_root
+    return @path_from_root unless @path_from_root.nil?
+    if @parent.nil?
+      @path_from_root = "root"
+    else
+      @path_from_root = File.join( @parent.path_from_root, @name )
+    end
+    @path_from_root
+  end
+
+  def basename
+    File.basename( @name )
   end
 
   def to_str
