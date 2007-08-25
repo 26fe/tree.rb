54f18e75aa4e25bb50ffec53b1714bbe9c6fb994
Tokiro <tokiro.oyama@gmail.com>
2007-08-25 07:51:29 +0000

    creata funzione per compilare i template kwartz
    moving from svn repository (dreamhost) to git

---
+++ lib/common/kwartzhelper.rb
@@ -0,0 +1,34 @@
+# ruby gems
+require "rubygems"
+require "kwartz"
+require "kwartz/main"
+
+#
+# kwartz utility
+#
+def compile( template_dir, template_include_dir, template_out_dir )
+
+  incs = Dir.new( template_include_dir ).find_all { |f| f =~ /\.html$/ }
+  incs = incs.map{ |f| File.join(template_include_dir, f) }.join(",")
+
+  Dir.foreach( template_dir ) { |f|
+    next unless f =~ /.html$/
+    inpath = File.join(template_dir, f)
+    outpath = File.join( template_out_dir, f.sub( /.html$/, ".rb" ) )
+    plogicpath = template_dir + File::Separator + f.sub( /.html$/, ".plogic" )
+
+    argv = %w[--delspan -a defun -l ruby -i]
+    argv.push( incs )
+
+    if File.exist?( plogicpath )
+      argv.push( "-p", plogicpath )
+    end
+    argv.push( inpath )
+
+    puts "kwarts " + argv.join(" ")
+    main = Kwartz::Main.new(argv)
+    output = main.execute()
+    File.open(outpath, 'w') { |f| f.write(output) }
+  }
+end
+
