2edc9d70ff6c8f8ac82db6ca87434870c32ad8b6
Tokiro <tokiro.oyama@gmail.com>
2007-09-09 08:15:46 +0000

    moving from svn repository (dreamhost) to git

--- lib/common/kwartzhelper.rb
+++ lib/common/kwartzhelper.rb
@@ -6,33 +6,49 @@ require "kwartz/main"
 #
 # kwartz utility
 #
-# TODO: compilare i file controllando le date
 def compile( template_dir, template_include_dir, template_out_dir )
 
+  maxtime = Time.local( 1980 ) 
   incs = []
   if File.directory?( template_include_dir ) 
     incs = Dir.new( template_include_dir ).find_all { |f| f =~ /\.html$/ }
-    incs = incs.map{ |f| File.join(template_include_dir, f) }.join(",")
+    incs = incs.map{ |f| File.join(template_include_dir, f) }
+    maxtime_incs = incs.inject(maxtime) {|t,f| 
+      t1 = File.mtime( f ) 
+      if t1 > t then t1 else t end
+    }
+    incs = incs.join(",")
   end
   
   Dir.foreach( template_dir ) { |f|
     next unless f =~ /.html$/
     inpath = File.join(template_dir, f)
-    outpath = File.join( template_out_dir, f.sub( /.html$/, ".rb" ) )
-    plogicpath = template_dir + File::Separator + f.sub( /.html$/, ".plogic" )
+    inpath_time = File.mtime( inpath )
 
+    plogicpath = template_dir + File::Separator + f.sub( /.html$/, ".plogic" )
+    plogic_time = Time.local( 1980 )
+    
+    outpath = File.join( template_out_dir, f.sub( /.html$/, ".rb" ) )
+    outpath_time = Time.local( 1980 )
+    if File.exist?( outpath )
+      outpath_time = File.mtime( outpath )
+    end
+    
     argv = %w[--delspan -a defun -l ruby -i]
     argv.push( incs )
 
     if File.exist?( plogicpath )
       argv.push( "-p", plogicpath )
+      plogicpath_time = File.mtime( plogicpath )
     end
     argv.push( inpath )
 
-    puts "kwartz " + argv.join(" ")
-    main = Kwartz::Main.new(argv)
-    output = main.execute()
-    File.open(outpath, 'w') { |f| f.write(output) }
+    if outpath_time < maxtime_incs or outpath_time < inpath_time or outpath_time < plogicpath_time 
+      puts "kwartz " + argv.join(" ")
+      main = Kwartz::Main.new(argv)
+      output = main.execute()
+      File.open(outpath, 'w') { |f| f.write(output) }      
+    end
   }
 end
 
