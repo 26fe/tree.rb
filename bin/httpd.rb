9c3d9635bfe924cfeabc8ed28133dc4b16d13355
Tokiro <tokiro.oyama@gmail.com>
2008-08-20 17:01:59 +0000

    up da vibo
    moving from svn repository (dreamhost) to git

---
+++ bin/httpd.rb
@@ -0,0 +1,23 @@
+#!/usr/local/bin/ruby
+
+$COMMON_HOME = File.expand_path( File.join( File.dirname( __FILE__), ".." ) )
+$:.unshift( File.join($COMMON_HOME, "lib" ) )
+
+require 'webrick'
+include WEBrick
+
+htdocs = File.join( $COMMON_HOME, "htdocs")
+
+s = HTTPServer.new(
+  :Port            => 2000,
+  :DocumentRoot    => htdocs
+)
+
+## mount subdirectories
+# s.mount("/ipr", HTTPServlet::FileHandler, "/proj/ipr/public_html")
+# s.mount("/~gotoyuzo",
+#         HTTPServlet::FileHandler, "/home/gotoyuzo/public_html",
+#        true)  #<= allow to show directory index.
+
+trap("INT"){ s.shutdown }
+s.start
