eea37ec1d1a63f30684533d7122eb76e3653c4f7
Giovanni Ferro <ferro@shuttle19.venere.it>
2009-05-20 18:43:36 +0200

    created new tree_visitor directory Moved file into the new directory

--- test/test_tree_visitor.rb
+++ test/test_tree_visitor.rb
@@ -6,8 +6,8 @@ $COMMON_HOME = File.expand_path( File.join( File.dirname( __FILE__), "..") )
 $:.unshift( File.join($COMMON_HOME, "lib" ) )
 $:.unshift( File.join($COMMON_HOME, "test" ) )
 
-require "ralbum-common"
-require "ralbum-common/tc_tree_node"
-require "ralbum-common/tc_tree_node_visitor"
-require "ralbum-common/tc_dir_tree_walker"
+require "tree_visitor"
+require "tree_visitor/tc_tree_node"
+require "tree_visitor/tc_tree_node_visitor"
+require "tree_visitor/tc_dir_tree_walker"
 # require "common/tc_dir_processor"
