a756dbfbf9fef7c601b69bb2589108e44b9e9764
Giovanni Ferro <giovanni.ferro@gmail.com>
2009-05-23 17:15:09 +0200

    moved all data used for test into a directory test_data now 'gem install -t' pass

--- test/tree_visitor/tc_dir_tree_walker.rb
+++ test/tree_visitor/tc_dir_tree_walker.rb
@@ -5,7 +5,7 @@ require 'test/unit'
 $TREE_VISITOR_HOME = File.expand_path( File.join( File.dirname( __FILE__), "..", ".." ) )
 $:.unshift( File.join($TREE_VISITOR_HOME, "lib" ) )
 $:.unshift( File.join($TREE_VISITOR_HOME, "test" ) )
-$TEST_DATA = File.join( $TREE_VISITOR_HOME, "test", "tree_visitor", "test_data" )
+$TEST_DATA = File.join( $TREE_VISITOR_HOME, "test_data", "tree_visitor", "test_data" )
 
 require 'tree_visitor/dir_tree_walker.rb'
 require 'tree_visitor/tree_node_visitor.rb'
