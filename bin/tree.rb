a6de0e6e4271ee2c67ee4adf51922543206122f3
Giovanni Ferro <giovanni.ferro@gmail.com>
2010-04-06 09:59:58 +0200

    erased gf_utilities code

--- bin/tree.rb
+++ bin/tree.rb
@@ -2,8 +2,8 @@
 #
 # Wrapper for the class CliTree
 
-$TREEVISITOR_HOME = File.expand_path( File.join( File.dirname( __FILE__), ".." ) )
-$:.unshift( File.join($TREEVISITOR_HOME, "lib" ) )
+TREEVISITOR_HOME = File.expand_path( File.join( File.dirname( __FILE__), ".." ) )
+$:.unshift( File.join(TREEVISITOR_HOME, "lib" ) )
 
 require "treevisitor/cli/cli_tree"
 exit CliTree.run
