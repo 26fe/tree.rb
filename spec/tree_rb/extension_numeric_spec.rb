c193bbb91f9c16f146e2a538f11b2e127792c249
Tokiro <tokiro.oyama@gmail.com>
2016-03-06 11:51:33 +0100

    fixed rspec warnings - should => expect().to

--- spec/tree_rb/extension_numeric_spec.rb
+++ spec/tree_rb/extension_numeric_spec.rb
@@ -5,9 +5,9 @@ require File.expand_path( File.join(File.dirname(__FILE__), "..", "spec_helper")
 describe "TCNumeric" do
 
   it "test_simple" do
-    10.with_separator.should == "10"
-    10.0.with_separator.should == "10.0"
-    1000000.with_separator.should == "1,000,000"
+    expect(10.with_separator).to be == "10"
+    expect(10.0.with_separator).to be == "10.0"
+    expect(1000000.with_separator).to be == "1,000,000"
   end
 
 end
