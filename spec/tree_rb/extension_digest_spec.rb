029588036dc57f9efc12dae3874eb3f4ab9ba81d
Tokiro <tokiro.oyama@gmail.com>
2016-03-06 11:51:18 +0100

    fixed rspec warnings - should => expect().to

--- spec/tree_rb/extension_digest_spec.rb
+++ spec/tree_rb/extension_digest_spec.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-require File.expand_path( File.join(File.dirname(__FILE__), "..", "spec_helper") )
+require File.expand_path( File.join(File.dirname(__FILE__), '..', 'spec_helper') )
 
 describe 'MD5' do
 
@@ -7,13 +7,12 @@ describe 'MD5' do
     @file_name = File.expand_path( File.join( File.dirname(__FILE__), "..", "..", "lib", "tree_rb", "extension_digest.rb" ) )
   end
 
-
   it 'should calculate md5' do
-    MD5.file( @file_name ).to_s.should  == 'b33c6b70109037fc02686f8babfc2db4'
+    expect(MD5.file( @file_name ).to_s).to be == 'b33c6b70109037fc02686f8babfc2db4'
   end
 
   it 'should calculate sha1' do
-    SHA1.file( @file_name ).to_s.should  == 'f99700dbdd200d7255f586ceb0cac05e05871cc5'
+    expect(SHA1.file( @file_name ).to_s).to be == 'f99700dbdd200d7255f586ceb0cac05e05871cc5'
   end
 
 end
