fabbdd7f80e7f07a0e09daed0677ce6597f8539d
Tokiro <tokiro.oyama@gmail.com>
2016-03-06 11:54:11 +0100

    fixed rspec warnings - should => expect().to

--- spec/tree_rb/input_file_system/directory_walker_conf_spec.rb
+++ spec/tree_rb/input_file_system/directory_walker_conf_spec.rb
@@ -5,55 +5,55 @@ describe DirTreeWalker do
 
   it 'should accept option :ignore with regex' do
     walker = DirTreeWalker.new :ignore => /^\./
-    walker.ignore_file?('.thumbnails').should be_true
-    walker.ignore_dir?('.thumbnails').should be_true
+    expect(walker.ignore_file?('.thumbnails')).to be true
+    expect(walker.ignore_dir?('.thumbnails')).to be true
   end
 
   it 'should accept option :ignore with string' do
     walker = DirTreeWalker.new :ignore => '.git'
-    walker.ignore_file?('.git').should be_true
-    walker.ignore_dir?('.git').should be_true
+    expect(walker.ignore_file?('.git')).to be true
+    expect(walker.ignore_dir?('.git')).to be true
   end
 
   it 'should accept option :ignore_dir' do
     dtw = DirTreeWalker.new :ignore_dir => [/^\./, "private_dir" ]
-    dtw.should be_ignore_dir '.git'
-    dtw.should be_ignore_dir 'private_dir'
+    expect(dtw.ignore_dir?('.git')).to be true
+    expect(dtw.ignore_dir?('private_dir')).to be true
   end
 
   it 'should accept option :ignore_file' do
     dtw = DirTreeWalker.new :ignore_file => [/.xml/, /(ignore)|(orig)/ ]
-    dtw.should be_ignore_file 'pippo.xml'
+    expect(dtw).to be_ignore_file('pippo.xml')
   end
 
   it 'should accept option :match with string' do
     dtw = DirTreeWalker.new :match => '.jpg'
-    dtw.should be_match 'foo.jpg'
+    expect(dtw).to be_match('foo.jpg')
   end
 
   it 'should accept option :match with regex' do
     dtw = DirTreeWalker.new :match => /.jpg/
-    dtw.should be_match 'foo.jpg'
+    expect(dtw).to be_match('foo.jpg')
   end
 
   it 'should ignore files and directory' do
     walker = DirTreeWalker.new('.')
 
     walker.ignore(/^\./)
-    walker.ignore_file?('.thumbnails').should be_true
-    walker.ignore_dir?('.thumbnails').should be_true
+    expect(walker.ignore_file?('.thumbnails')).to be true
+    expect(walker.ignore_dir?('.thumbnails')).to be true
 
     walker.ignore_dir('thumbnails')
-    walker.ignore_dir?('.thumbnails').should be_true
-    walker.ignore_dir?('thumbnails').should be_true
-    walker.ignore_dir?('pippo').should be_false
+    expect(walker.ignore_dir?('.thumbnails')).to be true
+    expect(walker.ignore_dir?('thumbnails')).to be true
+    expect(walker.ignore_dir?('pippo')).to be false
 
     walker.ignore_file('xvpics')
-    walker.ignore_file?('xvpics').should be_true
+    expect(walker.ignore_file?('xvpics')).to be true
 
     walker.ignore('sub')
-    walker.ignore_file?('[Dsube]').should be_false
-    walker.ignore_dir?('[Dsube]').should be_false
+    expect(walker.ignore_file?('[Dsube]')).to be false
+    expect(walker.ignore_dir?('[Dsube]')).to be false
   end
 
 end
