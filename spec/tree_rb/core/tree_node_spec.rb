2b08b67a95b208f94c37aecf921fb1c29cdbb641
Tokiro <tokiro.oyama@gmail.com>
2016-03-06 11:52:55 +0100

    fixed rspec warnings - should => expect().to

--- spec/tree_rb/core/tree_node_spec.rb
+++ spec/tree_rb/core/tree_node_spec.rb
@@ -5,35 +5,35 @@ describe TreeNode do
 
   it "should initialize correctly" do
     ta = TreeNode.new("a")
-    ta.should be_root
+    expect(ta).to be_root
 
     ln1 = LeafNode.new("1", ta)
-    ln1.parent.should == ta
-    ln1.should_not be_root
+    expect(ln1.parent).to be == ta
+    expect(ln1).to_not be_root
 
     ln2 = LeafNode.new("2", ta)
     tb  = TreeNode.new("b", ta)
     ln3 = LeafNode.new("3", tb)
-    ln3.parent.should == tb
+    expect(ln3.parent).to be == tb
 
     # test depth
-    tb.depth.should == 2
-    ln3.depth.should == 3
+    expect(tb.depth).to be == 2
+    expect(ln3.depth).to be == 3
 
     # test nr_nodes
-    tb.nr_nodes.should == 1
-    ta.nr_nodes.should == 4
+    expect(tb.nr_nodes).to be == 1
+    expect(ta.nr_nodes).to be == 4
 
     # puts ta.to_str
   end
 
   it "should add child and leaf" do
     ta = TreeNode.new("a")
-    ta.should be_root
+    expect(ta).to be_root
 
     ln1 = LeafNode.new("1")
     ta.add_leaf(ln1)
-    ln1.parent.should == ta
+    expect(ln1.parent).to be == ta
 
     ln2 = LeafNode.new("2", ta)
     ta.add_leaf(ln2)
@@ -41,7 +41,7 @@ describe TreeNode do
     tb  = TreeNode.new("b", ta)
     ln3 = LeafNode.new("3", tb)
     tb.add_leaf(ln3)
-    ln3.parent.should == tb
+    expect(ln3.parent).to be == tb
 
     ta.add_child(tb)
     # puts ta.to_str
@@ -49,27 +49,27 @@ describe TreeNode do
 
   it "next and prev" do
     ta = TreeNode.new("a")
-    ta.prev.should be_nil
-    ta.next.should be_nil
+    expect(ta.prev).to be_nil
+    expect(ta.next).to be_nil
 
     ln1 = LeafNode.new("1", ta)
-    ln1.prev.should be_nil
-    ln1.next.should be_nil
+    expect(ln1.prev).to be_nil
+    expect(ln1.next).to be_nil
 
     ln2 = LeafNode.new("2", ta)
-    ln2.prev.should == ln1
-    ln2.next.should be_nil
+    expect(ln2.prev).to be == ln1
+    expect(ln2.next).to be_nil
 
     ln3 = LeafNode.new("3", ta)
-    ln2.next.should == ln3
+    expect(ln2.next).to be == ln3
 
     tb = TreeNode.new("b", ta)
-    tb.next.should be_nil
-    tb.prev.should be_nil
+    expect(tb.next).to be_nil
+    expect(tb.prev).to be_nil
 
     tc = TreeNode.new("c", ta)
-    tc.prev.should == tb
-    tc.next.should be_nil
+    expect(tc.prev).to be == tb
+    expect(tc.next).to be_nil
   end
 
   context "navigate tree" do
@@ -84,37 +84,37 @@ describe TreeNode do
     end
 
     it "nr_nodes and nr_leaves and nr_children" do
-      @tree.nr_nodes.should == 5
-      @tree.nr_leaves.should == 4
-      @tree.nr_children.should == 1
+      expect(@tree.nr_nodes).to be == 5
+      expect(@tree.nr_leaves).to be == 4
+      expect(@tree.nr_children).to be == 1
 
-      @sub_tree.nr_nodes.should == 2
-      @sub_tree.nr_leaves.should == 2
-      @sub_tree.nr_children.should == 0
+      expect(@sub_tree.nr_nodes).to be == 2
+      expect(@sub_tree.nr_leaves).to be == 2
+      expect(@sub_tree.nr_children).to be == 0
     end
 
 
     context "find" do
 
       it "find by string" do
-        @tree.find("a").should === @tree
-        @tree.find("b").should === @sub_tree
-        @tree.find("3").should === @ln3
-        @tree.find("not existent").should be_nil
+        expect(@tree.find("a")).to be === @tree
+        expect(@tree.find("b")).to be === @sub_tree
+        expect(@tree.find("3")).to be === @ln3
+        expect(@tree.find("not existent")).to be_nil
       end
 
       it "find by regex" do
-        @tree.find(/[a,b]/).should === @tree
-        @tree.find(/[b,c]/).should === @sub_tree
-        @tree.find(/\d\d/).should === @ln4
-        @tree.find(/not existent/).should be_nil
+        expect(@tree.find(/[a,b]/)).to be === @tree
+        expect(@tree.find(/[b,c]/)).to be === @sub_tree
+        expect(@tree.find(/\d\d/)).to be === @ln4
+        expect(@tree.find(/not existent/)).to be_nil
       end
 
       it "find with block" do
-        @tree.find { |e| e.content == "a" }.should === @tree
-        @tree.find { |e| e.content == "b" }.should === @sub_tree
-        @tree.find { |e| e.content == "3" }.should === @ln3
-        @tree.find { |e| e.content == "not existent" }.should be_nil
+        expect(@tree.find { |e| e.content == "a" }).to be === @tree
+        expect(@tree.find { |e| e.content == "b" }).to be === @sub_tree
+        expect(@tree.find { |e| e.content == "3" }).to be === @ln3
+        expect(@tree.find { |e| e.content == "not existent" }).to be_nil
       end
 
       it "to_str" do
@@ -126,7 +126,7 @@ a
     |-- 3
     `-- 12
 EOS
-        @tree.to_str.should == out
+        expect(@tree.to_str).to be == out
       end
 
     end
