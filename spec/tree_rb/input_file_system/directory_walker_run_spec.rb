29a34e26cec0862163fcc2a2468f1ef8172dd8b1
Tokiro <tokiro.oyama@gmail.com>
2016-03-06 11:54:37 +0100

    fixed rspec warnings - should => expect().to

--- spec/tree_rb/input_file_system/directory_walker_run_spec.rb
+++ spec/tree_rb/input_file_system/directory_walker_run_spec.rb
@@ -5,35 +5,43 @@ describe DirTreeWalker do
 
   it 'should raise exception when run on inexistent directory' do
 
-    dtw = DirTreeWalker.new(match: /.jpg/)
-    root_dir = 'C:\GioProg\interzone\Dropbox\foto.altro\wallpapers\1920x1200_dell_16x10\color'
-    dtw.run root_dir do
-      on_leaf do |pathname|
-        puts "- #{pathname}"
+    captured = capture_output do
+      dtw = DirTreeWalker.new(match: /.jpg/)
+      root_dir = '/not_existent/'
+      dtw.run root_dir do
+        on_leaf do |pathname|
+          puts "- #{pathname}"
+        end
       end
     end
+    expected = /No such file or directory @ dir_initialize - \/not_existent\n/
+    expect(captured.err).to match expected
 
   end
 
   it 'should accumulate file names' do
+
     dir_tree_walker = DirTreeWalker.new(File.join(FIXTURES, "test_dir_1"))
 
     accumulator     = []
     visitor         = BlockTreeNodeVisitor.new { |pathname| accumulator << File.basename(pathname) }
     dir_tree_walker.run(visitor)
-    accumulator.length.should == 9
-    accumulator.sort.should == %w{ test_dir_1 dir.1 dir.1.2 file.1.2.1 file.1.1 dir.2 file.2.1 .dir_with_dot dummy.txt }.sort
+    expect(accumulator.length).to be == 9
+    expect(accumulator.sort).to be == %w{ test_dir_1 dir.1 dir.1.2 file.1.2.1 file.1.1 dir.2 file.2.1 .dir_with_dot dummy.txt }.sort
+
   end
 
   it 'should accumulate file names 2' do
+
     dir_tree_walker = DirTreeWalker.new(File.join(FIXTURES, "test_dir_2"))
     dir_tree_walker.ignore('sub')
 
     accumulator = []
     visitor     = BlockTreeNodeVisitor.new { |pathname| accumulator << File.basename(pathname) }
     dir_tree_walker.run(visitor)
-    accumulator.length.should == 2
-    accumulator.sort.should == %w{ [Dsube] test_dir_2 }.sort
+    expect(accumulator.length).to be == 2
+    expect(accumulator.sort).to be == %w{ [Dsube] test_dir_2 }.sort
+
   end
 
   it 'should ignore not accessible directory' do
@@ -44,16 +52,19 @@ describe DirTreeWalker do
     Dir.rmdir(f1) if File.exist?(f1)
     Dir.mkdir(f1, 0000)
 
-    f2 = File.join(dir, "accessible_dir")
+    f2 = File.join(dir, 'accessible_dir')
     Dir.rmdir(f2) if File.exist?(f2)
     Dir.mkdir(f2)
 
-    dir_tree_walker = DirTreeWalker.new(File.join(FIXTURES, "test_dir_3_with_error"))
+    dir_tree_walker = DirTreeWalker.new(File.join(FIXTURES, 'test_dir_3_with_error'))
     accumulator = []
     visitor     = BlockTreeNodeVisitor.new { |pathname| accumulator << File.basename(pathname) }
-    dir_tree_walker.run(visitor)
-    accumulator.length.should == 2
-    accumulator.sort.should == %w{accessible_dir test_dir_3_with_error }.sort
+
+    captured = capture_output do
+      dir_tree_walker.run(visitor)
+    end
+    expect(accumulator.length).to be == 2
+    expect(accumulator.sort).to be == %w{accessible_dir test_dir_3_with_error }.sort
 
     # Dir.rmdir(f1)
     # Dir.rmdir(f2)
--- spec/tree_rb/input_html_page/dom_walker_spec.rb
+++ spec/tree_rb/input_html_page/dom_walker_spec.rb
@@ -3,15 +3,24 @@ require File.join(File.dirname(__FILE__), "..", "..", "spec_helper")
 
 describe DomWalker do
 
-  it "should capture all files" do
+  it 'should capture all files' do
     value      = Nokogiri::HTML.parse(File.read(File.join(FIXTURES, "test_json", "1.json")))
     # pp value
     parent     = value.search("//body").first
     # recursive_search(parent)
 
     dom_walker = DomWalker.new(parent)
-    dom_walker.run(FlatPrintTreeNodeVisitor.new)
-    dom_walker.run(PrintTreeNodeVisitor.new)
+
+    captured = capture_output do
+      dom_walker.run(FlatPrintTreeNodeVisitor.new)
+    end
+    # puts captured.out
+
+    captured = capture_output do
+      dom_walker.run(PrintTreeNodeVisitor.new)
+    end
+    # puts captured.out
+
   end
 
 end
